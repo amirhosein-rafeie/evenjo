7ccad21d04fb0072423a4e3b8c479a46015d541a
Farnaz <farnazseyedi912@gmail.com>
2025-10-23 20:09:54 +0330

    feat: add Navbar, Footer and HomePage components

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,13 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import { BrowserRouter } from "react-router";
-import "./index.css";
-import App from "./App.jsx";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { Provider } from 'react-redux';
+import App from './App';
+import store from './store';
+import './index.css'
 
-createRoot(document.getElementById("root")).render(
-  <BrowserRouter>
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <Provider store={store}>
     <App />
-  </BrowserRouter>
+  </Provider>
 );
